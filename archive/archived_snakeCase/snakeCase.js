ea400d0216d4f9c7905e5657bd1805a9f62b7b38
roychrlstian <roylimin18@gmail.com>
2026-05-13 21:59:39 +0800

    Adds a logic for snakeCase
    but it is not completely done.

--- archive/archived_snakeCase/snakeCase.js
+++ archive/archived_snakeCase/snakeCase.js
@@ -1,5 +1,8 @@
-const snakeCase = function() {
+const snakeCase = function(str) {
+    const valid = 'abcdefghijklmnopqrstuvwxyz ';
+    let ans = str.toLowerCase().split("").filter(s => valid.includes(s)).join("").split(" ").join("_");
 
+    return ans;
 };
 
 // Do not edit below this line
